56e717327ee157fcace3d195927f30b4fc2ad3a1
M1LSS <117031759+M1LSS@users.noreply.github.com>
2025-06-20 02:42:16 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,9 +3,9 @@ const firebaseConfig = {
   authDomain: "gdp-project-7831b.firebaseapp.com",
   databaseURL: "https://gdp-project-7831b-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "gdp-project-7831b",
-  storageBucket: "gdp-project-7831b.appspot.com",
-  messagingSenderId: "",
-  appId: ""
+  storageBucket: "gdp-project-7831b.firebasestorage.app",
+  messagingSenderId: "66153805050",
+  appId: "1:66153805050:web:48ea3eefa8f2ae774a4543"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
