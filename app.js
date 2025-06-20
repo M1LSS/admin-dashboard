3dc4956aedbb5b41535238319b142d6bc4ede5e1
M1LSS <117031759+M1LSS@users.noreply.github.com>
2025-06-21 05:07:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Setup date picker (today and yesterday selectable)
   const today = new Date();
   const yyyy = today.getFullYear();
-  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const mm = String(today.getMonth() + 0).padStart(2, '0');
   const dd = String(today.getDate()).padStart(2, '0');
   const maxDate = `${yyyy}-${mm}-${dd}`;
 
