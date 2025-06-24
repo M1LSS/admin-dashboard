7d38852090c6d84927655170ced15624a9443ce1
M1LSS <117031759+M1LSS@users.noreply.github.com>
2025-06-25 01:51:23 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyAtbaEMPmljXmEfUU7k00obcIh4WVfDo3Y",
+  authDomain: "gdp-project-7831b.firebaseapp.com",
+  databaseURL: "https://gdp-project-7831b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gdp-project-7831b",
+  storageBucket: "gdp-project-7831b.appspot.com",
+  messagingSenderId: "66153805050",
+  appId: "1:66153805050:web:48ea3eefa8f2ae774a4543"
 };
 firebase.initializeApp(firebaseConfig);
