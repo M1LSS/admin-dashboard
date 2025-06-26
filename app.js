2c1344db1f6de2ef8972bec8a7156f441adc3489
M1LSS <117031759+M1LSS@users.noreply.github.com>
2025-06-26 15:38:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAtbaEMPmljXmEfUU7k00obcIh4WVfDo3Y",
-  authDomain: "gdp-project-7831b.firebaseapp.com",
-  databaseURL: "https://gdp-project-7831b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "gdp-project-7831b",
-  storageBucket: "gdp-project-7831b.appspot.com",
-  messagingSenderId: "66153805050",
-  appId: "1:66153805050:web:48ea3eefa8f2ae774a4543"
-};
-
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
